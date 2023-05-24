a4179cdaf7523b51aa0e9f5ccfeac7b545d5640d
Magnus Nordlander <magnus@nordlander.se>
2023-05-24 14:45:19 +0200

    No client certificates

--- docker-redis-entrypoint.sh
+++ docker-redis-entrypoint.sh
@@ -4,4 +4,5 @@ set -e
 redis-server --tls-port 6379 --port 0 \
     --tls-cert-file /tls/redis.crt \
     --tls-key-file /tls/redis.key \
-    --tls-ca-cert-file /tls/ca.crt
+    --tls-ca-cert-file /tls/ca.crt \
+    --tls-auth-clients no
